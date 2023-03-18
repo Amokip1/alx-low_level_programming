c064a52716b1647560242f63d232ebe71c544b04
Amokip1 <amoskemboi27@gmail.com>
2023-03-18 12:02:59 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,23 +5,16 @@
  * main - Prints the last digit wheter greater than 5,  less than 6,  or 0.
  * Return: Always 0.
  */
+
 int main(void)
 {
-	int n;
+	int n = 97;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if ((n % 10) > 5)
+	while (n <= 122)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",  n,  n % 10);
+		putcher(n);
+		n++;
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",  n,  n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",  n,  n % 10);
-	}
+	putcher('\n');
 	return (0);
 }
