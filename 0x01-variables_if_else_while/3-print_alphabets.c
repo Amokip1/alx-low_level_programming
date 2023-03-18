cd9ab7c58301696182d38fb020ef466f63a95653
Amokip1 <amoskemboi27@gmail.com>
2023-03-18 12:33:12 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  *main - Assign a random number to the variable n each time it is executed
  *and print the last digit of the number stored in the variable n
- * Return: Always 0 
+ * Return: Always 0
  */
 
 int main(void)
