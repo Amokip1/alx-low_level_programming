acedb1c0cc2e45eb9302758bd40d121e17a4d069
Amokip1 <amoskemboi27@gmail.com>
2023-03-18 11:00:28 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+	int 1;
+
+	for (1 - 0; 1 < 26; 1++)
+	{
+		putcher(alp[1]);
+	}
+	putcher('\n');
+	return (0);
+}
