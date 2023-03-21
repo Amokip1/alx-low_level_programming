ef366a2c62dbf8ce7cbf05b0de23f2c625cc81cf
Amokip1 <amoskemboi27@gmail.com>
2023-03-21 11:35:13 -0700

    prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1; i <= 10; i++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 97; j <= 122; j++)
+		for (i = 97; i <= 122; i++)
 		{
-			_putchar(j);
+			_putchar(i);
 		}
 		_putchar('\n');
 	}
