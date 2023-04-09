f684fb5093c390dac6c630766c11b5f08a5190fa
Amokip1 <amoskemboi27@gmail.com>
2023-04-09 01:40:24 -0700

    static library libmy.a

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetical letters
+ * @c: a charter to be checked
+ * Return: returns 1 if letter or 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 98))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
+}
